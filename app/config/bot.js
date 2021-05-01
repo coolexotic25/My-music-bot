e0968486eefec1052704d78ecdd1b92a141af323
coolexotic25 <66102155+coolexotic25@users.noreply.github.com>
2021-05-01 01:33:37 -0400

    Add files via upload

---
+++ app/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODI2OTA5Nzc5NTgxNzMwODU2.YGTVvg.lwIRCJwUxlVzKurxTOLVkgtITcg',
+        prefix: '^',
+        activity: 'With the creator Notcaylen0.0#2220',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
